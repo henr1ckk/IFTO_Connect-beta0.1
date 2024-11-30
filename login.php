b3fc5fd5714560bff64272b46310a3193aaffa31
Kaio Bezerra de Melo <108411594+KaioMelo78@users.noreply.github.com>
2024-11-30 20:54:39 -0300

    Update login.php

--- login.php
+++ login.php
@@ -10,46 +10,20 @@ if (isset($_POST['email']) && ($_POST['senha'])){
     $pass = mysqli_real_escape_string($conect, $pass);
 
     // Verificando se é aluno
-    $query_aluno = "SELECT id, nome, email, senha FROM alunos WHERE email = '$email'";
-    $result_aluno = mysqli_query($conect, $query_aluno);
-    $user_aluno = mysqli_fetch_assoc($result_aluno);
+    $query = "SELECT id, nome, email, senha, nivel FROM usuarios WHERE email = '$email'";
+    $result = mysqli_query($conect, $query);
+    $user = mysqli_fetch_assoc($result);
 
-    // Verificando se é professor
-    $query_professor = "SELECT id, nome, email, senha FROM professores WHERE email = '$email'";
-    $result_professor = mysqli_query($conect, $query_professor);
-    $user_professor = mysqli_fetch_assoc($result_professor);
-
-    // Verificando se é admin
-    $query_admin = "SELECT id, nome, email, senha FROM admins WHERE email = '$email'";
-    $result_admin = mysqli_query($conect, $query_admin);
-    $user_admin = mysqli_fetch_assoc($result_admin);
 
     // Verificação para Aluno
-    if ($user_aluno && $pass == $user_aluno['senha']) {
-        $_SESSION['iduser'] = $user_aluno['id'];
-        $_SESSION['username'] = $user_aluno['nome'];
-        $_SESSION['tipousuario'] = 'aluno';  // Define o tipo de usuário como aluno
+    if ($user && $pass == $user['senha']) {
+        $_SESSION['iduser'] = $user['id'];
+        $_SESSION['username'] = $user['nome'];
+        $_SESSION['nivel'] = $user['nivel'];  // Define o tipo de usuário como aluno
         header('location: home.php');
         exit;
     }
 
-    // Verificação para Professor
-    if ($user_professor && $pass == $user_professor['senha']) {
-        $_SESSION['iduser'] = $user_professor['id'];
-        $_SESSION['username'] = $user_professor['nome'];
-        $_SESSION['tipousuario'] = 'professor';  // Define o tipo de usuário como professor
-        header('location: home.php');
-        exit;
-    }
-
-    // Verificação para Admin
-    if ($user_admin && $pass == $user_admin['senha']) {
-        $_SESSION['iduser'] = $user_admin['id'];
-        $_SESSION['username'] = $user_admin['nome'];
-        $_SESSION['tipousuario'] = 'admin';  // Define o tipo de usuário como admin
-        header('location: home.php');
-        exit;
-    }
 
     // Caso as credenciais sejam inválidas
     $_SESSION['msg'] = 'Informações Inválidas!';
